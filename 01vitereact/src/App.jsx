2d2b7c6e80d891c28ffb6efe9151306fe8ba7abe
Aniket Hans <aniketdharya@gmail.com>
2024-11-24 00:03:09 +0530

    react basics

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import Name from "./Name";
+function App() {
+  return (
+    <>
+      <h1>Hello Duniya!!! from AH</h1>
+      <Name />
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/Name.jsx
@@ -0,0 +1,5 @@
+function Name() {
+  return <h1>Aniket Hans</h1>;
+}
+
+export default Name;
